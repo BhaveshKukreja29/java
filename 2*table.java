df21637fada7cccbfc0191dee465a449a6564df5
BhaveshKukreja29 <146556002+BhaveshKukreja29@users.noreply.github.com>
2024-07-11 14:58:20 +0530

    Create 2*table.java

---
+++ 2*table.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+class HelloWorld
+{
+	public static void main(String a[])
+	{
+        for (int i = 1; i < 11; i++)
+        {
+            System.out.println(2*i);
+        }
+	}
+}
