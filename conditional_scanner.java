2d97d4f560d059eb579bf548af426a096c73ed06
BhaveshKukreja29 <146556002+BhaveshKukreja29@users.noreply.github.com>
2024-07-18 16:00:58 +0530

    Create conditional_scanner.java

---
+++ conditional_scanner.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class hello
+{
+    public static void main(String affsaf[])
+    {
+        int marks;
+    
+        System.out.println("Welcome to grade calculator!");
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your overall percentage: ");
+        System.out.println("");
+        marks = input.nextInt();
+
+        System.out.println("Below is your grade!");
+        System.out.println("");
+
+        if (marks < 60)
+        {
+            System.out.println("F");
+        }
+    
+        else if (marks > 90)
+        {
+            System.out.println("A");
+        }
+        
+        else if (marks > 80)
+        {
+            System.out.println("B");
+        }
+        
+        else if (marks > 70)
+        {
+            System.out.println("C");
+        }
+        
+        else if (marks > 60)
+        {
+            System.out.println("D");
+        }
+    }
+}
