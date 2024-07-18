5167619631a171c5417b5471ebb994c2550a21e9
BhaveshKukreja29 <146556002+BhaveshKukreja29@users.noreply.github.com>
2024-07-18 16:32:19 +0530

    Create buffer2.java

---
+++ buffer2.java
@@ -0,0 +1,24 @@
+import java.lang.*;
+
+class buffer2
+{
+    public static void main(String a[])
+    {
+        System.out.println("The following is the count of number divisible by 7 which are greater than 100 and less than 200.");
+        System.out.println("");
+        int count = 0;
+        int sum = 0;
+
+        for (int i = 100; i < 201; i++)
+        {
+            if (i % 7 == 0)
+            {
+                sum += i;
+                count++;
+            }
+        }
+
+        System.out.println("Count is " + count);
+        System.out.println("Sum is " + sum);
+    }
+}
