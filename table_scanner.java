79b9b257c632db3187c26a5e8ae7cd706c84a701
BhaveshKukreja29 <146556002+BhaveshKukreja29@users.noreply.github.com>
2024-07-18 16:00:27 +0530

    Create table_scanner.java

---
+++ table_scanner.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class hello
+{
+	public static void main(String affsaf[])
+	{
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n = input.nextInt();
+        System.out.println("");
+          
+
+        for (int i = 1; i < 11; i++)
+        {
+            System.out.println(n + " x " + i + " = " + n*i);
+        }
+	}
+}
