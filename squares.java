cefc2099aaa31068845ab73df824be0042c3d6db
BhaveshKukreja29 <146556002+BhaveshKukreja29@users.noreply.github.com>
2024-07-18 16:09:48 +0530

    Create squares.java

---
+++ squares.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+
+class buffer1
+{
+    public static void main(String a[])
+    {
+        System.out.println("The following are the 25 squares.");
+        System.out.println("");
+
+        for (int i = 1; i < 26; i++)
+        {
+            System.out.println("The square of " + i + " is " + i*i);
+        }
+    }
+}
