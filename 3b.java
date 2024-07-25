1355d4fd64598f622734adbb5e0e85e148f5fe62
BhaveshKukreja29 <146556002+BhaveshKukreja29@users.noreply.github.com>
2024-07-25 16:35:27 +0530

    Create 3b.java

---
+++ 3b.java
@@ -0,0 +1,27 @@
+import java.lang.*;
+
+class Mult
+{
+    public static void main(String h[])
+    {
+        int i = 0;
+
+        int a = Integer.parseInt(h[i]);
+        i++;
+
+        int b = Integer.parseInt(h[i]);
+        i++;
+
+        int c = Integer.parseInt(h[i]);
+        i++;
+
+        System.out.println("Product first number is: " + a*5);
+        System.out.println();
+
+        System.out.println("Product second number is: " + b*5);
+        System.out.println();
+
+        System.out.println("Product third number is: " + c*5);
+        System.out.println();
+    }
+}
