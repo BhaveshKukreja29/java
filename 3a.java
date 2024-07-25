b10382ec2a4f3ab66c0b714c9ab02645193d097a
BhaveshKukreja29 <146556002+BhaveshKukreja29@users.noreply.github.com>
2024-07-25 16:27:03 +0530

    Create 3a.java

---
+++ 3a.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+
+class Info
+{
+    public static void main(String h[])
+    {
+        String name = h[0];
+        String roll =h[1];
+        String course = h[2];
+        String branch =  h[3];
+
+        System.out.println("The details of the Student are as follows");
+        System.out.println("Name: " + name);
+        System.out.println("Roll no.: " + roll);
+        System.out.println("Course: " + course);
+        System.out.println("Branch: " + branch);
+    }
+}
