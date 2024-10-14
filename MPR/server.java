2577038f9613fa33dc9bbfad98c5491fbe88c1f4
BhaveshKukreja29 <146556002+BhaveshKukreja29@users.noreply.github.com>
2024-10-14 08:59:06 +0530

    Update server.java

--- MPR/server.java
+++ MPR/server.java
@@ -6,18 +6,29 @@ import java.util.*;
 class Server
 {
     private static Map<Socket, String> clients = new HashMap<>();
+    private static boolean isRunning = true;
 
     public static void main(String args[]) throws Exception
     {
         ServerSocket ss = new ServerSocket(9806);
         System.out.println("Waiting for clients...");
 
-        while (true)
+        while (isRunning)
         {
-            Socket soc = ss.accept();
-            System.out.println("Connection Established. Current clients: " + clients.size());
-            new ClientHandler(soc).start();
+            try 
+            {
+                Socket soc = ss.accept();
+                System.out.println("Connection Established. Current clients: " + (clients.size() + 1));
+                new ClientHandler(soc).start();
+            } 
+            catch (SocketException e) 
+            {
+                System.out.println("Server socket closed.");
+            }
         }
+
+        ss.close();
+        System.out.println("Server successfully turned off.");
     }
 
     static class ClientHandler extends Thread
@@ -41,13 +52,15 @@ class Server
 
                 username = sbr.readLine();
                 clients.put(clientSocket, username);
-                broadcast(username, " joined the chat!!\n", true);
+                broadcast(" joined the chat!!\n", username, true);
+                System.out.println(username + " joined the chat.");
 
                 String msg;
                 while ((msg = sbr.readLine()) != null)
                 {
                     if (msg.equalsIgnoreCase("/quit")) 
                     {
+                        System.out.println(username + " left the server.");
                         broadcast(" left the chat.\n", username, true);
                         clients.remove(clientSocket);
                         break;
@@ -55,20 +68,35 @@ class Server
 
                     else if (msg.equalsIgnoreCase("/list"))
                     {
+                        System.out.println(username + " requested for usernames.");
                         for (Map.Entry<Socket, String> entry : clients.entrySet())
                         {
                             pw.println(entry.getValue());
                         }
                     }
 
-                    else if (msg.equalsIgnoreCase("/pm"))
+                    else if (msg.startsWith("/pm"))
                     {
-                        pw.println("Enter your friend's name: ");
-                        String friend = sbr.readLine().trim();
-                        pw.println("Enter a message for " + friend + ": ");
-                        String pmsg = sbr.readLine().trim();
+                        String[] pmsg = msg.split(" ", 3);
 
-                        pmessage(pmsg, friend, username);
+                        if (pmsg.length < 3)
+                        {
+                            pw.println("Incorrect format. Use /pm <their username> <message>");
+                        }
+
+                        String friend = pmsg[1];
+                        String msgp = pmsg[2];
+
+                        pmessage(msgp, friend, username);
+                    }
+
+                    else if (msg.equalsIgnoreCase("/help")) 
+                    {
+                        pw.println("Available commands:");
+                        pw.println("/list - Lists all connected users");
+                        pw.println("/pm <username> <message> - Sends a private message to a specific user");
+                        pw.println("/quit - Leave the chat");
+                        pw.println("To send a public message, just type and press enter.\n");
                     }
                     
                     else
@@ -81,6 +109,20 @@ class Server
             {
                 e.printStackTrace();
             }
+            finally
+            {
+                try
+                {
+                    if (clientSocket != null) 
+                    {
+                        clientSocket.close();  // Close the socket after the user leaves
+                    }
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
         }
 
         private void broadcast (String msg, String sender, boolean sys)
@@ -91,14 +133,14 @@ class Server
                 {
                     PrintWriter pw = new PrintWriter(entry.getKey().getOutputStream(), true);
 
-                    if (entry.getValue().equalsIgnoreCase(sender) && !sys)
+                    if (entry.getValue().equalsIgnoreCase(sender))
                     {
                         continue;   
                     }
 
                     if (sys)
                     {
-                        pw.println(msg + sender);
+                        pw.println(sender + msg);
                     }
                     else
                     {
@@ -114,6 +156,7 @@ class Server
 
         private void pmessage(String pmsg, String friend, String sender)
         {
+            boolean found = false;
             for (Map.Entry<Socket, String> entry : clients.entrySet())
             {
                 try
@@ -123,10 +166,24 @@ class Server
                     if (entry.getValue().equalsIgnoreCase(friend))
                     {
                         pw.println("Private " + sender + ": " + pmsg);
+                        found = true;
+                        break;
                     }
-
                 }
                 catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+
+            if (!found)
+            {
+                try
+                {   
+                    PrintWriter pw = new PrintWriter(clientSocket.getOutputStream(), true);
+                    pw.println("User '" + friend + "' not found.");
+                } 
+                catch (IOException e) 
                 {
                     e.printStackTrace();
                 }
