22ca2a5f9e1208f61c5c8ec04ca72eb0a6022bfc
BhaveshKukreja29 <146556002+BhaveshKukreja29@users.noreply.github.com>
2024-10-14 08:58:06 +0530

    Update client.java

--- MPR/client.java
+++ MPR/client.java
@@ -1,39 +1,39 @@
 import java.net.*;
 import java.io.*;
 
-class Client {
-    public static void main(String args[]) throws Exception {
-        System.out.println("Client instantiated.");
+class Client 
+{
+    public static void main(String args[]) throws Exception 
+    {
         Socket soc;
-        try
+        try 
         {
             soc = new Socket("localhost", 9806);
             System.out.println("Welcome to the chat!");
-        }
-        catch(IOException e)
+
+        } catch (IOException e) 
         {
             System.out.println("Error connecting to server. Please try later.");
             return;
         }
-    
+
         System.out.println("Chats below, enter *quit* to exit the chat.\n");
 
         InputStreamReader cisr = new InputStreamReader(System.in);
         BufferedReader cbr = new BufferedReader(cisr);
         PrintWriter pw = new PrintWriter(soc.getOutputStream(), true);
-
+        
         InputStreamReader sisr = new InputStreamReader(soc.getInputStream());
         BufferedReader sbr = new BufferedReader(sisr);
-
+        
         String username = "";
 
-        while (username.isEmpty())
+        while (username.isEmpty()) 
         {
             System.out.print("Enter username: ");
             username = cbr.readLine().trim();
-            System.out.println();
 
-            if (username.isEmpty())
+            if (username.isEmpty()) 
             {
                 System.out.println("Please enter a valid username.");
             }
@@ -42,25 +42,29 @@ class Client {
         pw.println(username);
 
         new Thread(() -> {
-            try {
+            try 
+            {
                 String msg;
-                while ((msg = sbr.readLine()) != null) {
+                while ((msg = sbr.readLine()) != null) 
+                {
                     System.out.println(msg);
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
+            } 
+            catch (IOException e) 
+            {
+                System.out.println(e);
             }
         }).start();
 
-        while (true) {
-            String message = cbr.readLine(); 
+        while (true) 
+        {
+            String message = cbr.readLine();
 
-            if (message.equalsIgnoreCase("/quit")) {
-                System.out.println("You left the chat server :(\nWe will miss you.");
+            if (message.equalsIgnoreCase("/quit")) 
+            {
                 pw.println(message);
                 break;
             }
-
             pw.println(message);
         }
 
